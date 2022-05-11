79a6f01976a61ab983232621c5706dfbd4bde7fe
Yajahira Velazquez <yari@yvelazquez.com>
2022-05-11 19:35:12 -0400

    added work/education, and loops

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 ### Lab 2
 
+>I began this project by creating 3 directories with their respective files to build a website all about me. I then wrote a simple HTML code, followed by the CSS code that turned my site from plain white to a vibrant maroon. Lastly, I created a JS file with 6 prompts, allowing the user to play a guessing game asking details about me.
+
 #### Today's Reflections
 
 >What I thought was going to be scary but easy to learn, simply because of my interest, I now realize will have areas where grey will be the norm. *What do I mean?* I am beginning to understand that in coding, while it may "appear" black and white, it isn't. While I had seen today's lesson in Code 102, today it clicked. There are ways around the "rules". That is the grey. 
@@ -13,3 +15,11 @@
 >If I can get through the lab with minimal help, then I know, and feel, I am on the right track.
 
 ### Lab 3
+
+>This lab was difficult! I was able to quickly add the required HTML requirements, then styling it in CSS. That part was fun and easy. The difficulty was the JS requirements. While I had write the 'for' ok, there were tidbits that I hadn't seen until it was pointed out to me. Fixed them, but then just as quickly broke the code. The array requirement was not working despite being written correctly. I believe the 'score' code is what held everything up.
+
+### Today's Reflections
+
+>In the military, when you become a sergeant they send you to school to learn how to be a leader, how to train other soldiers, and some administrative things. During these classes, they threw so much information, that it was impossible to keep it in your head. Knowing this would happen, they taught us the importance and strategy of searching through your resources. Everything nowadays is digital, so learning how to search pdf documents and customizing google searches became the norm.
+>
+>Having learned this skill, I think will help me find answers to my questions and doubts, faster and more accurately. This will also serve to allow myself the space for self-learning. It opens up the opportunity of learning through trial and error, leaving the option to get help for really big problems. 
--- js/app.js
+++ js/app.js
@@ -1,55 +1,103 @@
 'use strict'
 
+// let myArr = [answerOne, answerTwo, answerThree, answerFour, answerFive, answerSix, answerSeven];
+
 let answerOne = prompt('What\'s your name?');
 alert(`Hello ${answerOne}! Great to meet you!`);
 
+let score = 0;
+
 let answerTwo = prompt('Am I married?').toLowerCase();
 
-if(answerTwo ==='yes' || answerTwo === 'y'){
+if (answerTwo === 'yes' || answerTwo === 'y') {
   alert('Not for long!');
   // console.log ('You\'re correct!');
-} else if(answerTwo === 'no' || answerTwo === 'n') {
+  score++;
+} else if (answerTwo === 'no' || answerTwo === 'n') {
   alert('Hopefully by the end of this year, that\'ll be true.');
   // console.log('Its ok, you\'re not completely wrong.')
 }
 
 let answerThree = prompt('Was I in the military?').toLowerCase();
 
-if(answerThree === 'yes' || answerThree === 'y') {
+if (answerThree === 'yes' || answerThree === 'y') {
   alert('Yup! 17 years! HOOAH!');
   // console.log('Yup! 17 years! HOOAH!');
-} else if(answerThree === 'no' || answerThree === 'n') {
+  score++;
+} else if (answerThree === 'no' || answerThree === 'n') {
   alert('Whomp, whomp! Proudly served.');
   // console.log('Whomp, whomp! Proudly served.')
 }
 
 let answerFour = prompt('Did I go to medical school?').toLowerCase();
 
-if(answerFour === 'yes' || answerFour === 'y') {
+if (answerFour === 'yes' || answerFour === 'y') {
   alert('Sure did! Loved it, unitl question 2 happened.');
   // console.log('Sure did! Loved it, until question 2 happened.');
-} else if(answerFour === 'no' || answerFour === 'n') {
-  alert('Let me check your blood sugar, \'cause your vision must be blurry.');
-  // console.log('Let me check your blood sugar, \'cause your vision must be blurry.');
+  score++;
+} else if (answerFour === 'no' || answerFour === 'n') {
+  alert('I sure did! Let me check your blood sugar.');
+  // console.log('Let me check your blood sugar.');
 }
 
 let answerFive = prompt('Do I have children?').toLowerCase();
 
-if(answerFive === 'yes' || answerFive === 'y') {
+if (answerFive === 'yes' || answerFive === 'y') {
   alert('A boy and a girl...teenagers!');
   // console.log('A boy and a girl...teenagers!');
-} else if(answerFive === 'no' || answerFive === 'n') {
+  score++;
+} else if (answerFive === 'no' || answerFive === 'n') {
   alert('Yeah I do!');
   // console.log('Yeah I do!');
 }
 
-let answerSix = prompt('Did you enjoy this little encounter?').toLowerCase();
+let answerSix = prompt('Are you ready for more?').toLowerCase();
 
-if(answerSix === 'yes' || answerSix === 'y') {
+if (answerSix === 'yes' || answerSix === 'y') {
   alert('Me too!');
   // console.log('Me too!');
-} else if(answerSix === 'no' || answerSix === 'n') {
-  alert('Well...I\'m not for everyone.');
-  // console.log('Well...I\'m not for everyone.')
+  score++;
+} else if (answerSix === 'no' || answerSix === 'n') {
+  alert('Come on! This fun!');
+  // console.log('Come on! This fun!)
 }
+
+for (let i = 0; i < 4; i++) {
+  let answerSeven = prompt('How old do you think I am?').toLowerCase();
+
+  if (answerSeven == 41) {
+    alert('That\'s right!');
+    // console.log('That\'s right!');
+    score++;
+    {break;}
+  } else if (answerSeven > 41) {
+    alert('Too high!');
+    // console.log('Too high!');
+  } else if (answerSeven < 41) {
+    alert('Too low!');
+    // console.log('Too low!');
+  }
+}
+
+alert('I\'m 41 years young!');
+
+let myArr = ['mexican', 'cuban', 'korean'];
+for (let i = 0; i < 6; i++) {
+  let answerEight = prompt('What is my favorite food?').toLowerCase();
+
+  if (answerEight == 'mexican') {
+    alert('That\'s right!');
+    // console.log('That\'s right!');
+    score++;
+    {break};
+  } else if (answerEight !== 'mexican') {
+    alert('Whomp! Whomp!');
+    // console.log('Whomp! Whomp!');
+  }
+}
+
+alert('I\'m puertorican, but I LOVE MEXICAN FOOD!');
+// console.log('I\'m puertorican, but I LOVE MEXICAN FOOD!');
+alert(score);
+// console.log(score);
 
