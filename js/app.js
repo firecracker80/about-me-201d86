68a5de99789c89ae099719549677212b04960cb6
Yajahira Velazquez <yari@yvelazquez.com>
2022-05-12 19:29:15 -0400

    added parentheses to function

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@
 
 // let myArr = [answerOne, answerTwo, answerThree, answerFour, answerFive, answerSix, answerSeven];
 
-function q1 {
+function q1() {
   let answerOne = prompt('What\'s your name?');
   alert(`Hello ${answerOne}! Great to meet you!`);
 }
 
 q1();
+
 let score = 0;
 
-function q2 {
-
+function q2() {
   let answerTwo = prompt('Am I married?').toLowerCase();
   
   if (answerTwo === 'yes' || answerTwo === 'y') {
@@ -23,10 +23,10 @@ function q2 {
     // console.log('Its ok, you\'re not completely wrong.')
   }
 }
+
 q2();
 
-function q3 {
-
+function q3() {
   let answerThree = prompt('Was I in the military?').toLowerCase();
   
   if (answerThree === 'yes' || answerThree === 'y') {
@@ -41,8 +41,7 @@ function q3 {
 
 q3();
 
-function q4 {
-
+function q4() {
   let answerFour = prompt('Did I go to medical school?').toLowerCase();
   
   if (answerFour === 'yes' || answerFour === 'y') {
@@ -54,11 +53,10 @@ function q4 {
     // console.log('Let me check your blood sugar.');
   }
 }
+
 q4();
 
-
-function q5 {
-
+function q5() {
   let answerFive = prompt('Do I have children?').toLowerCase();
   
   if (answerFive === 'yes' || answerFive === 'y') {
@@ -70,10 +68,10 @@ function q5 {
     // console.log('Yeah I do!');
   }
 }
+
 q5();
 
-function q6 {
-
+function q6() {
   let answerSix = prompt('Are you ready for more?').toLowerCase();
   
   if (answerSix === 'yes' || answerSix === 'y') {
@@ -88,49 +86,49 @@ function q6 {
 
 q6();
 
-
-function q7 {
+function q7() {
   for (let i = 0; i < 4; i++) {
-  let answerSeven = prompt('How old do you think I am?').toLowerCase();
+    let answerSeven = prompt('How old do you think I am?').toLowerCase();
 
-  if (answerSeven == 41) {
-    alert('That\'s right!');
-    // console.log('That\'s right!');
-    score++;
-    {break;}
-  } else if (answerSeven > 41) {
-    alert('Too high!');
-    // console.log('Too high!');
-  } else if (answerSeven < 41) {
-    alert('Too low!');
-    // console.log('Too low!');
-  }
-}
-alert('I\'m 41 years young!');
-}
-q7();
-
-
-
-function q8 {
-let myArr = ['mexican', 'steak', 'chinese'];
-for (let i = 0; i < 6; i++) {
-  let answerEight = prompt('What is my favorite food?').toLowerCase();
-  for(let j = 0; j < myArr.length; j++)
-
-    if (answerEight === myArr[j]) {
+    if (answerSeven == 41) {
       alert('That\'s right!');
       // console.log('That\'s right!');
       score++;
-      i=6
-      {break};
+      {break;}
+    } else if (answerSeven > 41) {
+      alert('Too high!');
+    // console.log('Too high!');
+    } else if (answerSeven < 41) {
+      alert('Too low!');
+    // console.log('Too low!');
     }
+  }
+  alert('I\'m 41 years young!');
 }
-alert(`My favorite foods are ${myArr}.`)
-// console.log(`My favorite foods are ${myArr}.`);
-alert(`Your score is ${score}.`);
+
+q7();
+
+function q8() {
+  let myArr = ['mexican', 'steak', 'chinese'];
+  for (let i = 0; i < 6; i++) {
+    let answerEight = prompt('What is my favorite food?').toLowerCase();
+    for(let j = 0; j < myArr.length; j++)
+
+      if (answerEight === myArr[j]) {
+        alert('That\'s right!');
+        // console.log('That\'s right!');
+        score++;
+        i=6;
+        {break};
+      }
+  }
+
+  alert(`My favorite foods are ${myArr}.`)
+  // console.log(`My favorite foods are ${myArr}.`);
+  alert(`Your score is ${score}.`);
 // console.log(score);
 }
+
 q8();
 
 
